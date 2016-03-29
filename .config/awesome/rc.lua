3ce8836d6747c2b3feaa3962f36d0f8a3e2c0bbf
Ildar Akhmetgaleev <akhilman@gmail.com>
2016-03-29 07:17:01 +0700

    alt+tab and back to W+space to layout changing

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -289,6 +289,16 @@ globalkeys = awful.util.table.join(
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
         end),
+    awful.key({ "Mod1",           }, "Tab",
+        function ()
+            awful.client.focus.byidx( 1)
+            if client.focus then client.focus:raise() end
+        end),
+    awful.key({ "Mod1", "Control" }, "Tab",
+        function ()
+            awful.client.focus.byidx(-1)
+            if client.focus then client.focus:raise() end
+        end),
     awful.key({ "Control",        }, "Escape", function () mymainmenu:show() end),
 
     -- Layout manipulation
@@ -316,8 +326,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
     awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "w", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "w", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
+    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
