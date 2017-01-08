cd99d2dbca5dee79d211e1c3668a8446af5ac7f6
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-08 17:00:14 +0700

    Defining clock format and update interval

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -145,7 +145,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock()
+mytextclock = awful.widget.textclock(" %a %b %d, %H:%M ", 1)
 mytestclock_tooltip = awful.tooltip({
     objects = { mytextclock },
     timer_function = function()
